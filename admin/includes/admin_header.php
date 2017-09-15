062fe9af3503d066831ec82e19d0249048380b79
asliabir <abiruzzaman.molla@gmail.com>
2017-09-15 09:57:58 +0600

    Adding Dynamic Chart in Admin Deshboard

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -33,6 +33,7 @@ if(!is_admin($_SESSION['username'])){
         <!-- jQuery -->
         <script src="js/jquery.js"></script>
         <script src="js/scripts.js"></script>
+        <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
     </head>
 
     <body>
