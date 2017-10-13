a54f033b09117d2a1e76edb05949b1d708e90310
Md. Abiruzzaman Molla <abiruzzaman.molla@gmail.com>
2017-10-13 21:39:43 +0600

    adding database items

---
+++ admin/install/create_database.php
@@ -0,0 +1,29 @@
+$sql = "--\n"
+
+    . "-- Database: `id231601_cms`\n"
+
+    . "--\n"
+
+    . "\n"
+
+    . "-- --------------------------------------------------------\n"
+
+    . "\n"
+
+    . "--\n"
+
+    . "-- Table structure for table `categories`\n"
+
+    . "--\n"
+
+    . "\n"
+
+    . "CREATE TABLE `categories` (\n"
+
+    . "  `cat_id` int(11) NOT NULL,\n"
+
+    . "  `cat_title` varchar(255) NOT NULL,\n"
+
+    . "  `cat_image` varchar(255) NOT NULL DEFAULT \'no_category.png\'\n"
+
+    . ") ENGINE=InnoDB DEFAULT CHARSET=latin1";
